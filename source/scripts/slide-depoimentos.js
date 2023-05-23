0ed12c83b8b17f202b52ee2408529cbd7b2cb3b7
heitor augusto <heitoraugusto8@hotmail.com>
2023-05-23 13:59:29 -0300

    corrigindo os botoes do depoimento e alinhando a section VUV

--- source/scripts/slide-depoimentos.js
+++ source/scripts/slide-depoimentos.js
@@ -4,91 +4,75 @@ const nextButton = document.querySelector('.carrossel-botao.next');
 
 let currentItem = 0;
 
-nextButton.addEventListener('click', handleNextSlide)
-previousButton.addEventListener('click', handlePreviousSlide)
+nextButton.addEventListener('click', handleNextSlide);
+previousButton.addEventListener('click', handlePreviousSlide);
 
+function handleNextSlide() {
+  const temProximoSlide = currentItem < carouselItem.length - 1;
 
-function handleNextSlide(){
-  let ultimoSlide
-  const temProximoSlide = carouselItem[currentItem] != carouselItem[carouselItem.length-1]
-  // //Validação
-  
-  if(!temProximoSlide){
-    return
+  if (!temProximoSlide) {
+    return;
   }
 
-  // //Ação
-  nextSlide()
+  nextSlide();
 
-  // //Resultado
-  if (carouselItem[currentItem] === carouselItem[carouselItem.length-1]){
-    ultimoSlide = carouselItem[currentItem]
-  }
+  document.querySelector('.seta-direita').src = "build/images/arrow-right.svg";
 
-  if(ultimoSlide){
-     //add atribute "disabled"
-      nextButton.setAttribute('disabled', true)
-      
-      document.querySelector('.seta').src = "images/arrow-right-black.svg"
-      
+  if (carouselItem[currentItem] === carouselItem[carouselItem.length - 1]) {
+    nextButton.setAttribute('disabled', true);
+    document.querySelector('.seta-direita').src = "build/images/arrow-right-black.svg";
+    previousButton.removeAttribute('disabled');
+  } else {
+    nextButton.removeAttribute('disabled');
+    previousButton.removeAttribute('disabled');
+    document.querySelector('.seta').src = "build/images/arrow-left-white.svg";
   }
-       //remove atribute disabled do previousButton
-      previousButton.removeAttribute('disabled')
-      document.querySelector('.seta').src = "images/arrow-left-white.svg"
 }
 
-function handlePreviousSlide(){
-  let primeiroSlide
-const temAnteriorSlide = carouselItem[currentItem] != carouselItem[0]
+function handlePreviousSlide() {
+  const temAnteriorSlide = currentItem > 0;
 
- //Validação
-
-  if(!temAnteriorSlide){
-    return
+  if (!temAnteriorSlide) {
+    return;
   }
 
-  // //Ação
-  previousSlide()
+  previousSlide();
 
-  // //Resultado
-  if (carouselItem[currentItem] === carouselItem[0]){
-    primeiroSlide = carouselItem[currentItem]
+  if (carouselItem[currentItem] === carouselItem[0]) {
+    previousButton.setAttribute('disabled', true);
+    nextButton.removeAttribute('disabled');
+    document.querySelector('.seta').src = "build/images/arrow-left.svg";
+  } else {
+    previousButton.removeAttribute('disabled');
+    nextButton.removeAttribute('disabled');
+    document.querySelector('.seta-esquerda').src = "build/images/arrow-left-white.svg";
+    document.querySelector('.seta-direita').src = "build/images/arrow-right.svg";
   }
 
-  if(primeiroSlide){
-    // add atribute "disabled"
-  previousButton.setAttribute('disabled', true)
-  document.querySelector('.seta').src = "images/arrow-right-black.svg"
-
-  }
-   //remove atribute disbled do nextButton
-  nextButton.removeAttribute('disabled')
-  document.querySelector('.seta').src = "images/arrow-left-white.svg"
 }
 
-
-function nextSlide(){
-  currentItem++
-  carouselItem.forEach(item =>{
-    item.classList.remove("ativo")
-  })
-  carouselItem[currentItem].classList.add('ativo')
+function nextSlide() {
+  currentItem++;
+  carouselItem.forEach((item) => {
+    item.classList.remove("ativo");
+  });
+  carouselItem[currentItem].classList.add('ativo');
   carouselItem[currentItem].scrollIntoView({
     behavior: "smooth",
-    inline:"start",
+    inline: "start",
     block: "nearest"
-  })
+  });
 }
 
-function previousSlide(){
-  currentItem--
-  carouselItem.forEach(item =>{
-    item.classList.remove("ativo")
-  })
-  carouselItem[currentItem].classList.add('ativo')
+function previousSlide() {
+  currentItem--;
+  carouselItem.forEach((item) => {
+    item.classList.remove("ativo");
+  });
+  carouselItem[currentItem].classList.add('ativo');
   carouselItem[currentItem].scrollIntoView({
     behavior: "smooth",
-    inline:"start",
+    inline: "start",
     block: "nearest"
-  })
+  });
 }
